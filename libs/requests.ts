1c29cecc9e48096ffc2e59b8185874a84ca98c13
RBX Family <49787648+my-store@users.noreply.github.com>
2025-08-29 08:56:03 +0700

    Update requests.ts
    Menambahkan fitur upload file

--- libs/requests.ts
+++ libs/requests.ts
@@ -1,3 +1,6 @@
+import { Platform } from "react-native";
+
+// Change this in production
 export const serverUrl: string = "http://192.168.1.103:5000";
 
 export async function JSONGet(
@@ -24,4 +27,58 @@ export async function JSONPost(
   });
   const res = await req.json();
   return res;
+}
+
+export async function FormPost(
+  url: string,
+  options?: RequestInit
+): Promise<any> {
+  const body: any = options?.body;
+
+  // No body presented
+  if (!body) {
+    return null;
+  }
+
+  const data: any = new FormData();
+  for (let key in body) {
+    // String value (default)
+    let val: any = body[key];
+
+    // File value
+    if (typeof val != "string") {
+      // Terminate if no assets | file presented
+      if (!val.assets || val.assets.length < 1) continue;
+
+      // Get file data
+      const { fileName, mimeType, uri } = val.assets[0];
+
+      // Terminate if file data is unknown
+      if (!fileName || !mimeType || !uri) continue;
+
+      // Set file upload format
+      val = {
+        name: fileName,
+        type: mimeType,
+        uri:
+          Platform.OS === "android"
+            ? // Android is full path
+              uri
+            : // IOS remove file protocol
+              uri.replace("file://", ""),
+      };
+    }
+
+    data.append(key, val);
+  }
+
+  return JSONPost(url, {
+    ...options,
+    headers: {
+      ...options?.headers,
+      "Content-Type": "multipart/form-data",
+      Accept: "application/json",
+    },
+    body: data,
+  });
 }
